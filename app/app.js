b386c89929024d6bb004620db289a04347138083
Christopher Sanson <christophersanson@gmail.com>
2019-07-01 12:34:47 -0400

    Update app.js
    bump to test build triggers

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var app = express()
 app.get('/', require('./routes/index'))
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+  console.log('Sample app listening on port 3000!')
 })
 
 module.exports = app
